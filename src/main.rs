23987e8fe2c3a5eee8439ddd03352858c0f75e81
ld000 <lidong9144@163.com>
2021-12-22 02:03:31 +0800

    add post

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
-use reqwest::Url;
+use reqwest::{Client, Url};
+use anyhow::Result;
 
 /// A naive httpie implementation with Rust, can you imagine how easy it is?
 #[derive(Parser, Debug)]
@@ -18,6 +19,7 @@ enum SubCommand {
 /// feed get with an url and we will retrieve the response for you
 #[derive(Parser, Debug)]
 struct Get {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
 }
 
@@ -26,6 +28,7 @@ struct Get {
 #[derive(Parser, Debug)]
 struct Post {
     /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// HTTP 请求的 body
     body: Vec<String>,
@@ -36,7 +39,27 @@ fn parse_url(s: &str) -> Result<String> {
     Ok(s.into())
 }
 
-fn main() {
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
+
     println!("{:?}", opts);
+
+    let client = Client::new();
+    let _result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await,
+        SubCommand::Post(ref args) => post(client, args).await,
+    };
+
+    Ok(())
 }
